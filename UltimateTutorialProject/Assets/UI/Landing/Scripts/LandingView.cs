a93d4b500ccc28fe8a4bb8283675dc047eb78593
Stanislav Osipov <lacost.st@gmail.com>
2019-12-24 01:13:21 +0200

    Landing Scene setup

---
+++ UltimateTutorialProject/Assets/UI/Landing/Scripts/LandingView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+using Unity.UIElements.Runtime;
+
+namespace com.stansassets.ut
+{
+    public class LandingView : MonoBehaviour
+    {
+        [SerializeField] private PanelRenderer m_PanelRenderer = null;
+
+        private void Start()
+        {
+            Debug.Log("Start");
+            var root = m_PanelRenderer.panel.visualTree;
+            root.Query<Button>().ForEach(button =>
+            {
+                Debug.Log(button);
+            });
+        }
+    }
+}
+
+
